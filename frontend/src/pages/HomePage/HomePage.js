20593ca468e02882373e9346d78677f1bf13f045
andyrod <andyrod209@gmail.com>
2022-03-20 10:35:37 -0700

    fix: Post now display from newest to oldest.

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,27 +9,31 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   // const [user, token] = useAuth();
-  const [posts, setPosts] = useState([]);
-  
+  const [posts, setPosts] = useState([{}]);
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         let response = await axios.get("http://127.0.0.1:8000/api/posts/view_posts/");
-        console.log(response.data);
-        setPosts(response.data)
+        let data = response.data
+        setPosts(data);
+       console.log(data)
+
       } catch (error) {
         console.log(error.message);
       }
     };
     fetchPosts();
   }, []);
+
+  // try to find a way to have new post display after someone creates a new one. 
   return (
     <div className="container">
       <h1>Home Page for test</h1>
       <CreatePost />
-      <>{posts.map((post, index) => {
+      <>{[...posts].reverse().map((post, index) => {
         return (
-          <div className="post" key={index}>
+            <div className="post" key={index}>
           <ul>
               <li>Username: {post.user}</li>
               <li>Post: {post.text}</li>
