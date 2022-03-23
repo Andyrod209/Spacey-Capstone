fdeeeb9c98383330da7058f2009fac8190c0585d
andyrod <andyrod209@gmail.com>
2022-03-22 20:39:17 -0700

    Chore: like and dislike both toggle, APOD is uploaded on the home page.

---
+++ frontend/src/components/ToggleButton/ToggleButton.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import './ToggleButton.css'
+
+const ToggleButton = () => {
+
+    const[like, setLike] = useState('normal')
+    const[dislike, setDislike] = useState('normal')
+
+    function handleLikeClick (){
+        if (like === 'normal'){
+            setLike('like');
+        }
+        else {
+            setLike('normal')
+        }
+        if (dislike === "dislike"){
+            setDislike("normal")
+        }
+    }
+
+    function handleDislikeClick (){
+        if (dislike === 'normal'){
+            setDislike('dislike');
+        }
+        else {
+            setDislike('normal')
+        }
+        if (like === 'like') {
+            setLike('normal');
+        }
+    }
+
+    function thumbsUpOrDown(x){
+        x.classList.toggle("fa-thumbs-down");
+      }
+
+    return ( 
+        <div>
+            
+            <button className={like} type='button' onClick={handleLikeClick}>
+                <i onclick={() => thumbsUpOrDown(this)} className="fa fa-thumbs-up"></i>
+            </button> 
+            <button className={dislike} type='button' onClick={handleDislikeClick}>
+                <i onclick={() => thumbsUpOrDown(this)} className="fa fa-thumbs-down"></i>
+            </button>
+        </div>
+     );
+}
+ 
+export default ToggleButton;
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,13 +5,16 @@ import useAuth from "../../hooks/useAuth";
 import "./HomePage.css";
 import CreatePost from "../../components/CreatePost/CreatePost";
 import EditPost from "../../components/EditPost/EditPost";
+import keys from "./API_Keys.json"
+import ToggleButton from "../../components/ToggleButton/ToggleButton";
 
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
   const [posts, setPosts] = useState([{}]);
-  const [postId, setPostId] = useState(Number)
+  const [postId, setPostId] = useState(Number);
+  const [apod, setApod] = useState();
 
   const fetchPosts = async () => {
     try {
@@ -26,6 +29,7 @@ const HomePage = () => {
   
   useEffect(() => {
     fetchPosts();
+    getAPOD();
   }, [token]);
 
   async function postDelete(id){
@@ -39,15 +43,36 @@ const HomePage = () => {
     console.log(error.response);
 }
 };
+
+  // increnment = () => {
+  //   let newcount = 
+  //   let post = {
+  //     "user": user.user_id,
+  //     "text": postInfo,
+  //     "likes": 0,
+  //     "dislikes": 0
+  // };
+  // let response = await axios.put(`http://127.0.0.1:8000/api/posts/edit/${postId}/`, post,
+  //   { headers: {Authorization: 'Bearer ' + token,}}); 
+  //     await fetchPosts();
+  //     console.log(response.data);
+  // }
   
   let getId = (id) => {
     setPostId(id)
     console.log(id)
-    }; 
+    };
+    
+    async function getAPOD(){
+      let response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${keys.NasaApiKey}`)
+      console.log(response)
+      setApod(response.data.url)
+    }
  
   return (
     <div className="container">
       <h1>Home Page for test</h1>
+      <img src={apod} alt='Picture of the day'/>
       <CreatePost setPost={setPosts} posts={posts} fetchPosts = {fetchPosts} />
       <EditPost postId={postId} fetchPosts = {fetchPosts}/>
       <>{[...posts].reverse().map((post, id) => {
@@ -65,8 +90,8 @@ const HomePage = () => {
           <ul>
               <li>Username: {post.user}</li>
               <li>Post: {post.text}</li>
-              <li>Likes {post.likes}</li>
-              <li>Dislikes {post.dislikes}</li>
+              
+              <ToggleButton />
               <button onClick={() => getId(post.id)}>Edit</button>
               <button onClick={()=> postDelete(post.id)}>DELETE</button>
           </ul>
