8676a43e16e5661a07b86c6d6324990da9a84137
andyrod <andyrod209@gmail.com>
2022-03-22 20:58:15 -0700

    test

--- frontend/src/pages/CommentsPage/CommentsPage.js
+++ frontend/src/pages/CommentsPage/CommentsPage.js
@@ -1,9 +1,9 @@
 const CommentsPage = () => {
-
-    // theory to how to get done with comments:
     // use a get request to get the information and some how get just that single post
         // and have comments under the post 
     // this will be a new page
+
+    
     
     return ( 
         <div>
