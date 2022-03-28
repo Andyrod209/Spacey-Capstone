cb1ca8daf834569b57e102cd07d627a4eac9d623
andyrod <andyrod209@gmail.com>
2022-03-27 21:31:32 -0700

    style: Changed profile to look a bit more nicer

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -6,26 +6,37 @@ const ProfilePage = () => {
 
     const [user, token] = useAuth()
     const [profiles, setProfiles] = useState([])
-    const [profileChange, setProfileChange] = useState()
+    const [profileChange, setProfileChange] = useState('')
     console.log(profileChange)
     useEffect(() => {
         getProfile();
     }, [token]);
 
     async function getProfile(){
-        let response = await axios.get(`http://127.0.0.1:8000/api/profiles/edit/${user.user_id}/`, { headers: 
+        let response = await axios.get(`http://127.0.0.1:8000/api/profiles/edit/${user.id}/`, { headers: 
         {Authorization: 'Bearer ' + token,},
         })
         setProfiles(response.data)
     }
+    
+    // async function CreateAbout(){
+    //     let about = {
+    //         user: user.user_id,
+    //         about: profileChange 
+    //     };
+    //     let response = await axios.put(`http://127.0.0.1:8000/api/profiles/`, about, { headers: 
+    //     {Authorization: 'Bearer ' + token,},})
+    //     console.log(response) 
+    //     await getProfile()
+    // }
 
     async function editProfile(){
         let about = {
-            user: user.user_id,
+            user: user.id,
             about: profileChange 
         };
-        let response = await axios.put(`http://127.0.0.1:8000/api/profiles/edit/${user.user_id}/`, about, { headers: 
-        {Authorization: 'Bearer ' + token,},})
+        let response = await axios.put(`http://127.0.0.1:8000/api/profiles/edit/${user.id}/`, about, { headers: 
+        {Authorization: 'Bearer ' + token}})
         console.log(response) 
         await getProfile()
     }
@@ -37,13 +48,13 @@ const ProfilePage = () => {
 
     return ( 
         <div className="container">
-            <h1>{user.username}</h1>
-                <h3>Profile</h3>
+            <h1 style={{color:'beige'}}>{user.username}</h1>
+                <h3 style={{color:'beige'}}>Profile</h3>
                 <img src="https://i.ibb.co/pdJfzx9/profile-picture.jpg" alt="profile-picture" border="0" />
-            <h4>About Me</h4>
+            <h3 style={{color:'beige'}}>About Me :</h3>
+                <p style={{color:'beige'}}>{profiles.about}</p>
                 <input type='textbox' onChange={(event) => setProfileChange(event.target.value)}/>
                 <button type="submit" onClick={handleSubmit}>Change</button>
-                <p>{profiles.about}</p>
                 <br/>
         <div className="listFlex">
         </div>
